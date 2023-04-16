1f0ecb06452b5d2b1fd85ce77dc38c0bbe5f03af
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2023-04-16 04:08:50 +0200

    -Change Main Window class components -moved open widgets inside a new Qwidget class -added fileopen.py

--- UI/windows/mainframe.py
+++ UI/windows/mainframe.py
@@ -1,9 +1,8 @@
 import sys
-from pathlib import Path
+from UI.windows.widgets import fileopen
 
 from PyQt6 import QtGui
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QListWidget, QLabel, QGridLayout, QLineEdit, \
-    QFileDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
 class MainWindow(QMainWindow):
@@ -14,29 +13,13 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QtGui.QIcon("Proteus.png"))
         self.setGeometry(450, 130, 800, 600)
 
-        add = QPushButton("Add File",self)
-        add.setGeometry(20, 20, 100, 40)
-        add.setCheckable(True)
-        add.clicked.connect(self.open_file)
-
-        self.file_list = QLineEdit(self)
-        self.file_list.setGeometry(140,25,300,30)
-
+        self.open = fileopen.uiopen()
+        self.layout().addWidget(self.open)
 
     def closeEvent(self, event):
         for window in QApplication.topLevelWidgets():
             window.close()
             exit()
-
-    def open_file(self):
-        filename, ok = QFileDialog.getOpenFileName(
-            self,
-            "Select a File",
-            "Images (*.png *.jpg)"
-        )
-        if filename:
-            path = Path(filename)
-            self.file_list.setText(str(path))
 
 
 app = QApplication(sys.argv)
---
+++ UI/windows/widgets/fileopen.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from PyQt6.QtWidgets import QPushButton, QLineEdit, QFileDialog, QWidget
+
+class uiopen(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.add_section()
+
+    def add_section(self):
+        #Button
+        add = QPushButton("Add File", self)
+        add.setGeometry(20, 20, 100, 40)
+        add.clicked.connect(self.open_file)
+
+        #Path
+        self.file_list = QLineEdit(self)
+        self.file_list.setGeometry(140, 25, 300, 30)
+
+    def open_file(self):
+        filename, ok = QFileDialog.getOpenFileName(
+            self,
+            "Select a File",
+            "Images (*.png *.jpg)"
+        )
+        if filename:
+            path = Path(filename)
+            self.file_list.setText(str(path))
